3419eca84329b9e0356c0836e4e20cb02b08761e
Sam Kowash <kowash@mox2.hyak.local>
2018-10-17 16:33:32 -0700

    preliminary data

---
+++ HW1/code/mox_batch.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+## Job Name
+
+#SBATCH --job-name=mnist_ridge
+
+## Allocation Definition
+## Which queue do you want to use?
+
+#SBATCH --partition=stf
+
+## Number of simulations that are being done. (Max number is 28?)
+## By default 1 CPU core per task is used. Can be changed with --ntasks
+
+#SBATCH --tasks=16
+
+## How many nodes are you using?
+
+#SBATCH --nodes=1
+
+## Walltime needed (Days-Hours:Minutes:Seconds)
+
+#SBATCH --time=0-1:30:00
+
+## Memory per node
+
+#SBATCH --mem=100G
+
+## Email beginning and end of job
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=kowash@uw.edu
+
+## Specify the working directory for this job (Where is job located?)
+
+#SBATCH --workdir=/gscratch/home/kowash/ml_class/HW1/code
+
+##START WORK SECTION
+
+
+
+START=$(date +%s.%N)
+module load anaconda3_4.3.1
+mapfile -t indices < /gscratch/home/kowash/ml_class/HW1/code/index_list
+for p in "${indices[@]}"; do
+    python mnist_ridge.py $p &
+done
+wait
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo $DIFF
