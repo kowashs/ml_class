2bd257833af5cd8e4cde9200963d19191e8c3cdc
Sam Kowash <kowash@uw.edu>
2018-10-23 16:41:56 -0700

    Removed big files, added to gitignore

--- HW2/code/data/load_example.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-# Load a csv of floats:
-X = np.genfromtxt("upvote_data.csv", delimiter=",")
-# Load a text file of integers:
-y = np.loadtxt("upvote_labels.txt", dtype=np.int)
-# Load a text file of strings:
-featureNames = open("upvote_features.txt").read().splitlines()
