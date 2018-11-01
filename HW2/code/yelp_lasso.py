eedc44eaa281eff59fc7d8b05230ecfedb135695
Sam Kowash <kowash@uw.edu>
2018-11-01 02:01:22 -0700

    Lasso param adjustment

--- HW2/code/yelp_lasso.py
+++ HW2/code/yelp_lasso.py
@@ -20,10 +20,10 @@ X_train = X[:4000]
 Y_train = np.sqrt(Y[:4000])
 
 X_val = X[4000:5000]
-Y_val = Y[4000:5000]
+Y_val = np.sqrt(Y[4000:5000])
 
 X_test = X[5000:]
-Y_test = Y[5000:]
+Y_test = np.sqrt(Y[5000:])
 
 
 lam_max = np.max(2*np.abs(np.matmul(Y_train - np.mean(Y_train), X_train)))
@@ -37,7 +37,7 @@ ws = []
 bs = []
 
 lam = lam_max
-r = .95
+r = .98
 
 print("Entering loop.")
 while (max(num_feats) if num_feats else 0) < .95*d:
@@ -47,11 +47,11 @@ while (max(num_feats) if num_feats else 0) < .95*d:
     bs.append(b)
 
 
-    val_pred = (np.matmul(X_val, np.transpose(w)) + b)**2
-    train_pred = (np.matmul(X_train, np.transpose(w)) + b)**2
+    val_pred = np.matmul(X_val, np.transpose(w)) + b
+    train_pred = np.matmul(X_train, np.transpose(w)) + b
 
-    val_diff = val_pred - Y_val
-    train_diff = train_pred - Y_train**2
+    val_diff = val_pred - Y_val 
+    train_diff = train_pred - Y_train
     
     feats = np.count_nonzero(w)
     val_err = np.matmul(val_diff, val_diff)
@@ -60,13 +60,13 @@ while (max(num_feats) if num_feats else 0) < .95*d:
     num_feats.append(feats)
     val_errs.append(val_err)
     train_errs.append(train_err)
-    print(f"{lam:10e} {feats:6d} {val_err:8.2f} {train_err:8.2f}")
+#   print(f"{lam:12e} {feats:4d} {val_err:14.6f} {train_err:14.6f}")
 
     lam *= r
 
 
 
-with open("data/yelp_results", "w") as f:
+with open("data/yelp_sqrt_results", "w") as f:
     f.writelines([f"{lams[i]:12e} {num_feats[i]:4d} {val_errs[i]:14.6f} {train_errs[i]:14.6f}\n" for i in range(len(lams))])
 
 
